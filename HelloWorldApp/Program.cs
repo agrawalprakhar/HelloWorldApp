69e5c49036189cd0f92255e9350644a0be0766fb
Prakhar <agrawalprakhar893@gmail.com>
2023-08-16 12:11:32 +0530

    Add Multiply method

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloWorldApp
+class Program
 {
-    internal class Program
+    // Method to calculate the sum of two integers
+    static int Sum(int a, int b)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
-        }
+        return a + b;
+    }
+    static int Multiply(int a, int b)
+    {
+        return (a * b);
+    }
+    static void Main(string[] args)
+    {
+        // Call the Sum method with two integers and print the result
+        int num1 = 5;
+        int num2 = 7;
+        int Add = Sum(num1, num2);
+        int multiply=Multiply(num1, num2);
+
+        Console.WriteLine($"The sum of {num1} and {num2} is: {Add}");
+        Console.WriteLine($"The Multiplication of {num1} and {num2} is {multiply}");
+        Console.ReadLine();
     }
 }
